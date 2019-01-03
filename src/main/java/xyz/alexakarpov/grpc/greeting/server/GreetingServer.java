e2daa41a92caf597d25ff92b9e3a7ec18a173f19
Alexandre Karpov <alexakarpov@gmail.com>
2019-01-02 19:40:57 -0500

    bummer

---
+++ src/main/java/xyz/alexakarpov/grpc/greeting/server/GreetingServer.java
@@ -0,0 +1,24 @@
+package xyz.alexakarpov.grpc.greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello gRPC");
+        Server server = ServerBuilder.forPort(50051)
+                .build();
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received shutdown request");
+            server.shutdown();
+            System.out.println("We're all done");
+        }));
+
+        server.awaitTermination();
+    }
+}
