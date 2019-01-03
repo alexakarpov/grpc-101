8aa92f14cd3fbe819dd97b80f8df4fa58b233819
Alexandre Karpov <alexakarpov@gmail.com>
2019-01-03 12:51:39 -0500

    debug server creation a bit more

--- src/main/java/xyz/alexakarpov/grpc/greeting/server/GreetingServer.java
+++ src/main/java/xyz/alexakarpov/grpc/greeting/server/GreetingServer.java
@@ -6,19 +6,30 @@ import io.grpc.ServerBuilder;
 import java.io.IOException;
 
 public class GreetingServer {
+    public static long time() {
+        return System.currentTimeMillis();
+    }
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println(String.format("Hello from %d, gRPC",
-                Thread.currentThread().getId()));
+        System.out.println(String.format("Hello from %d at %d",
+                Thread.currentThread().getId(),
+                time()));
         Server server = ServerBuilder.forPort(50051)
+                .addService(new GreetServiceImpl())
                 .build();
-        System.out.println(String.format("Starting the server at %d", Thread.currentThread().getId()));
+        System.out.println(String.format("Starting the server on %d at %d",
+                Thread.currentThread().getId(),
+                time()));
         server.start();
-        System.out.println(String.format("Will this print - at %d?", Thread.currentThread().getId()));
+        System.out.println(String.format("Will this print - on %d at %d?", Thread.currentThread().getId(),
+                time()));
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-             System.out.println(String.format("Received shutdown request in %d",
-                     Thread.currentThread().getId()));
+             System.out.println(String.format("Received shutdown request in %d at %d",
+                     Thread.currentThread().getId(),
+                     time()));
             server.shutdown();
-            System.out.println(String.format("We're all done here in %d", Thread.currentThread().getId()));
+            System.out.println(String.format("We're all done here in %d at %d",
+                    Thread.currentThread().getId(),
+                    time()));
         }));
 
         System.out.println(String.format("How about this? - at %d", Thread.currentThread().getId()));
