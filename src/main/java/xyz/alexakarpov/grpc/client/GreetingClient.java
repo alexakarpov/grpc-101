8703570395e5f7d02308e4e76b7fe38cbaf0a4c7
Alexandre Karpov <alexakarpov@gmail.com>
2019-01-03 12:50:03 -0500

    refactor message dance

--- src/main/java/xyz/alexakarpov/grpc/client/GreetingClient.java
+++ src/main/java/xyz/alexakarpov/grpc/client/GreetingClient.java
@@ -2,20 +2,46 @@ package xyz.alexakarpov.grpc.client;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import xyz.alexakarpov.proto.greet.*;
 
 public class GreetingClient {
     public static void main(String[] args) {
-        System.out.println("Hi from client");
         ManagedChannel chan = ManagedChannelBuilder
                 .forAddress("localhost", 50051)
-//                .usePlaintext()
+                .usePlaintext()
                 .build();
 
         System.out.println("Creating stubs");
+        GreetServiceGrpc.GreetServiceBlockingStub syncGreetClient = GreetServiceGrpc.newBlockingStub(chan);
 
-        // do something
+        doMessageDance(syncGreetClient, "Alex", "Foo");
+        doMessageDance(syncGreetClient, "Bill", "Bar");
+        doMessageDance(syncGreetClient, "Karl", "Baz");
 
         System.out.println("Shutting channel down");
         chan.shutdown();
+    }
+
+    private static void doMessageDance(GreetServiceGrpc.GreetServiceBlockingStub syncGreetClient,
+                                       String firstName,
+                                       String lastName) {
+        Greeting g;
+        GreetRequest r;
+        GreetResponse response;
+        System.out.println("Building a Greeting");
+        g = Greeting.newBuilder()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .build();
+
+        System.out.println("Building a GreetRequest");
+        r = GreetRequest.newBuilder()
+                .setGreeting(g)
+                .build();
+
+        System.out.println("Sending the RPC");
+        response = syncGreetClient.greet(r);
+
+        System.out.println(String.format("Received %s", response.getResult()));
     }
 }
