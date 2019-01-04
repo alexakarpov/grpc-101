46d9a80d09a8659b9c2eb06a96aa18e633f6ee8a
Alexandre Karpov <alexakarpov@gmail.com>
2019-01-04 12:50:12 -0500

    cleintside

--- src/main/java/xyz/alexakarpov/grpc/client/GreetingClient.java
+++ src/main/java/xyz/alexakarpov/grpc/client/GreetingClient.java
@@ -17,17 +17,19 @@ public class GreetingClient {
 
         GreetServiceGrpc.GreetServiceBlockingStub syncGreetClient = GreetServiceGrpc.newBlockingStub(chan);
 
-        doMessageDance(syncGreetClient, "Alex", "Foo");
-        doMessageDance(syncGreetClient, "Bill", "Bar");
-        doMessageDance(syncGreetClient, "Karl", "Baz");
+        doGreetingDance(syncGreetClient, "Alex", "Foo");
+        doGreetingDance(syncGreetClient, "Bill", "Bar");
+        doGreetingDance(syncGreetClient, "Karl", "Baz");
 
+        System.out.println("Now let's do some addition");
+        doAddDance(syncGreetClient, 21, 21);
         System.out.println("Shutting channel down");
         chan.shutdown();
     }
 
-    private static void doMessageDance(GreetServiceGrpc.GreetServiceBlockingStub syncGreetClient,
-                                       String firstName,
-                                       String lastName) {
+    private static void doGreetingDance(GreetServiceGrpc.GreetServiceBlockingStub syncGreetClient,
+                                        String firstName,
+                                        String lastName) {
         Greeting g;
         GreetRequest r;
         GreetResponse response;
@@ -49,4 +51,30 @@ public class GreetingClient {
                 timeStr(),
                 response.getResult()));
     }
+
+    private static void doAddDance(GreetServiceGrpc.GreetServiceBlockingStub syncGreetClient,
+                                        int firstNum,
+                                        int secondNum) {
+        Addition a;
+        AddRequest r;
+        AddResponse response;
+        a = Addition.newBuilder()
+                .setFirstNum(firstNum)
+                .setSecondNum(secondNum)
+                .build();
+
+        r = AddRequest.newBuilder()
+                .setAddition(a)
+                .build();
+
+        System.out.println(String.format("Sending the RPC at %s with request:\n%s",
+                timeStr(),
+                r.toString()));
+        response = syncGreetClient.add(r);
+
+        System.out.println(String.format("Received response at %s\n%s",
+                timeStr(),
+                response.getResult()));
+    }
+
 }
